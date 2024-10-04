4558f8add63917138a9bcc353a19e38c44a01f60
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2024-10-04 11:43:32 +0530

    Update page.tsx

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -14,13 +14,12 @@ const agendaList = [
         "speaker": ""
       },
       {
-        "title": "Abirami Sukumaran",
+        "title": "Building Agentic Applications with Cloud Databases and Serverless Runtimes",
         "time": "9:30 AM - 10:15 AM",
-        "speaker": "Abirami Sukumaran",
-        "topic": "Building Agentic Applications with Cloud Databases and Serverless Runtimes"
+        "speaker": "Abirami Sukumaran"
       },
       {
-        "title": "Kiran S Raj",
+        "title": "About Google Developer Groups",
         "time": "10:20 AM - 10:55 AM",
         "speaker": "Kiran S Raj"
       },
@@ -30,10 +29,9 @@ const agendaList = [
         "speaker": ""
       },
       {
-        "title": "Arun Teja Godavarthi",
+        "title": "Gemini",
         "time": "11:25 AM - 11:40 AM",
-        "speaker": "Arun Teja Godavarthi",
-        "topic": "Gemini"
+        "speaker": "Arun Teja Godavarthi"
       },
       {
         "title": "Brief Overview of the Event",
@@ -41,10 +39,9 @@ const agendaList = [
         "speaker": ""
       },
       {
-        "title": "Rejin Jose K",
+        "title": "Through My Eyes: A Developer Retrospective",
         "time": "12:05 PM - 12:40 PM",
-        "speaker": "Rejin Jose K",
-        "topic": "Through My Eyes: A Developer Retrospective"
+        "speaker": "Rejin Jose K"
       },
       {
         "title": "Lunch break",
@@ -66,7 +63,6 @@ const agendaList = [
         "time": "6:00 PM - 6:20 PM",
         "speaker": ""
       }
-
     ]
   },
   {
@@ -78,28 +74,24 @@ const agendaList = [
         "speaker": ""
       },
       {
-        "title": "Sachin Kumar",
+        "title": "Building Powerful AI Agents using Vertex AI Agent Builder",
         "time": "2:20 PM - 2:55 PM",
-        "speaker": "Sachin Kumar",
-        "topic": "Building Powerful AI Agents using Vertex AI Agent Builder"
+        "speaker": "Sachin Kumar"
       },
       {
-        "title": "Varun Raj",
+        "title": "Building AI Apps with Firebase Genkit and Gemini",
         "time": "3:05 PM - 3:40 PM",
-        "speaker": "Varun Raj",
-        "topic": "Building AI Apps with Firebase Genkit and Gemini"
+        "speaker": "Varun Raj"
       },
       {
-        "title": "Dana Vishnu",
+        "title": "Web Security Essentials for Developers",
         "time": "3:50 PM - 4:25 PM",
-        "speaker": "Dana Vishnu",
-        "topic": "Web Security Essentials for Developers"
+        "speaker": "Dana Vishnu"
       },
       {
-        "title": "Deepu Xavier, Nithin Suresh",
+        "title": "Generative AI: Revolutionizing Product Development",
         "time": "4:35 PM - 5:10 PM",
-        "speaker": "Deepu Xavier, Nithin Suresh",
-        "topic": "Generative AI: Revolutionizing Product Development"
+        "speaker": "Deepu Xavier, Nithin Suresh"
       }
     ]
   },
@@ -112,32 +104,29 @@ const agendaList = [
         "speaker": ""
       },
       {
-        "title": "Anubhav Singh",
+        "title": "Exploring Goroutines as Cognitive Threads in Go",
         "time": "2:20 PM - 2:55 PM",
-        "speaker": "Anubhav Singh",
-        "topic": "Exploring Goroutines as Cognitive Threads in Go"
+        "speaker": "Anubhav Singh"
       },
       {
-        "title": "Abhishek Sharma",
+        "title": "Unleashing the Potential of GKE for Next-Level Solutions",
         "time": "3:05 PM - 3:40 PM",
-        "speaker": "Abhishek Sharma",
-        "topic": "Unleashing the Potential of GKE for Next-Level Solutions"
+        "speaker": "Abhishek Sharma"
       },
       {
-        "title": "Aayush Arora",
+        "title": "Streamlining GenAI with Google Cloud Workflows",
         "time": "3:50 PM - 4:25 PM",
-        "speaker": "Aayush Arora",
-        "topic": "Streamlining GenAI with Google Cloud Workflows"
+        "speaker": "Aayush Arora"
       },
       {
-        "title": "Biswanath Giri",
+        "title": "Securing the Future: Safeguarding GenAI Systems",
         "time": "4:35 PM - 5:10 PM",
-        "speaker": "Biswanath Giri",
-        "topic": "Securing the Future: Safeguarding GenAI Systems"
+        "speaker": "Biswanath Giri"
       }
     ]
   }
 ];
+
 
 
 export default function Agenda() {
@@ -153,7 +142,7 @@ export default function Agenda() {
               {track.sessions.map((session, sessionIndex) => (
                 <div key={sessionIndex} className="flex flex-col items-center gap-4">
                   <AccordionItem 
-                    title={session.title} 
+                    title={session.topic} 
                     time={session.time} 
                     speaker={session.speaker}
                   />
